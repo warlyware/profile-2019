7be69ffe7638117c6a35f26b3e28710485b81b1b
Dan Ward <danward@gmail.com>
2019-03-29 14:32:33 -0400

    Convert typed content to declarative flow

--- components/TypedText.vue
+++ components/TypedText.vue
@@ -8,25 +8,46 @@ import TypeIt from 'typeit'
 export default {
   data() {
     return {
-      strings: [
-        'Hi!',
-        'My name is Dan Ward.',
-        `I'm a web developer,`,
-        'author, technical writer,',
-        'and web technology consultant.',
-        '',
-        'I live and work',
-        'in Virginia Beach, VA',
-        'at Marathon Consulting.'
-      ]
+      typingSpeed: {
+        Brisk: 50,
+        Slow: 200
+      }
     }
   },
   mounted() {
     new TypeIt('#typed-text', {
-      strings: this.strings,
-      speed: 50,
-      nextStringDelay: 200
-    }).go()
+      speed: this.typingSpeed.Brisk,
+      nextStringDelay: 300
+    }).type('Hi!')
+      .pause(500)
+      .break()
+      .type(`My name is `)
+      .options({ speed: this.typingSpeed.Slow })
+      .type('<span class="text-red">Dan Ward</span>.')
+      .options({ speed: this.typingSpeed.Brisk })
+      .pause(500)
+      .break()
+      .type(`I'm a web developer,`)
+      .break()
+      .type('author, technical writer,')
+      .break()
+      .type('and web technology consultant.')
+      .pause(500)
+      .exec(async () => {
+        await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            return resolve()
+          }, 2000)
+        })
+      })
+      .break()
+      .break()
+      .type('I live and work')
+      .break()
+      .type('in Virginia Beach, VA')
+      .break()
+      .type('at Marathon Consulting.')
+      .go()
   }
 }
 </script>
