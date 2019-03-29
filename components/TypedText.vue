5f2d6124ab06933b68c41209402d74aafc0ac01d
Dan Ward <danward@gmail.com>
2019-03-29 14:17:14 -0400

    Move typed-text to own component

---
+++ components/TypedText.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="typed-text" class="font-bold text-5xl"></div>
+</template>
+
+<script>
+import TypeIt from 'typeit'
+
+export default {
+  data() {
+    return {
+      strings: [
+        'Hi!',
+        'My name is Dan Ward.',
+        `I'm a web developer,`,
+        'author, technical writer,',
+        'and web technology consultant.',
+        '',
+        'I live and work',
+        'in Virginia Beach, VA',
+        'at Marathon Consulting.'
+      ]
+    }
+  },
+  mounted() {
+    new TypeIt('#typed-text', {
+      strings: this.strings,
+      speed: 50,
+      nextStringDelay: 200
+    }).go()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,16 @@
 <template>
-  <section class="container px-16 py-16 flex items-end">
-    <div id="typed-text" class="font-bold text-5xl"></div>
+  <section class="px-16 py-16 flex items-end min-h-screen">
+    <typed-text />
   </section>
 </template>
 
 <script>
-import TypeIt from 'typeit'
+import TypedText from '~/components/TypedText.vue'
 
 export default {
-  data() {
-    return {
-      strings: [
-        'Hi!',
-        'My name is Dan Ward.',
-        `I'm a web developer,`,
-        'author, technical writer,',
-        'and web technology consultant.',
-        '',
-        'I live and work',
-        'in Virginia Beach, VA',
-        'at Marathon Consulting.'
-      ]
-    }
-  },
-  components: { },
-  mounted() {
-    new TypeIt('#typed-text', {
-      strings: this.strings,
-      speed: 50,
-      nextStringDelay: 200
-    }).go()
-  }
+  components: { TypedText }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-}
 </style>
