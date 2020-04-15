6e74af673fc9ed12ed0719e49bf85252b25b4c84
Dan Ward <danward@gmail.com>
2020-04-14 17:55:41 -0700

    Move proficiency modal to own component

--- components/Proficiencies.vue
+++ components/Proficiencies.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="px-8 md:px-32 py-16 flex flex-col justify-center items-center h-screen">
-    <modal name="proficiency-deail" transition="modal">
-      hello, world!
-    </modal>
+    <proficiency-detail-modal />
     <h2 class="self-center text-3xl uppercase mb-4 md:mt-8 font-light">
       Proficiencies
     </h2>
@@ -11,9 +9,9 @@
       class="tracking-wide md:text-xl lg:text-3xl uppercase absolute h-full w-full overflow-y-scroll flex flex-wrap text-justify md:text-center">
         <div v-for="proficiency in proficiencies"
         :key="proficiency"
-        class="p-2 lg:p-4 flex-grow"
-        @click="showModal(proficiency)">
-          <span class="cursor-pointer p-2 bg-opaque hover:bg-alt-blue-2 hover:text-brown-dark ">
+        class="p-2 lg:p-4 flex-grow">
+          <span class="cursor-pointer p-2 bg-opaque hover:bg-alt-blue-2 hover:text-brown-dark"
+          @click="showModal(proficiency)">
             {{ proficiency }}
           </span>
         </div>
@@ -23,12 +21,17 @@
 </template>
 
 <script>
+import ProficiencyDetailModal from '~/components/modals/ProficiencyDetailModal.vue'
+
 export default {
+  components: {
+    ProficiencyDetailModal
+  },
   data() {
     return {
       proficiencies: [
-        'React',
         'Vue',
+        'React',
         'Redux',
         'Vuex',
         'NgRx',
---
+++ components/modals/ProficiencyDetailModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <modal id="modal" height="auto"
+  width="40%" max-width="50"
+  classes="bg-alt-blue-2"
+  name="proficiency-deail" transition="modal">
+    <div class="bg-opaque-brown-dark">
+      hello, world!
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/css/colors.scss';
+</style>
